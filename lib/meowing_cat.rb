b86b134a087dfd374e9826483a13ac8b5cf784b8
patiencewanjiru <patiencewanjiru123@gmail.com>
2023-05-22 13:22:04 +0300

    Completed assignment

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,17 @@
 ## code your solution here. 
+class Cat
+    attr_accessor :name
+    
+    def meow
+        puts "meow!"
+    end
+end
+
+# Create an instance of Cat
+my_cat = Cat.new
+
+# Name of the cat
+my_cat.name = "Whiskers"
+
+# Call the meow method
+my_cat.meow
